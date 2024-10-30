db863de8b133fb500e7d75c417440b68e1a1dbc7
Thi08fast <160783786+Thi08fast@users.noreply.github.com>
2024-10-30 15:10:34 -0300

    Piedra papel tijera

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,41 @@
+import random
 
-num1 = 1
-num2 =1
-sum = num1 + num2
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+def piedra_papel_tijera():
+    opciones = ["piedra", "papel", "tijera"]
     
+    while True:
+       
+        print("\nElige una opción:")
+        print("1. Piedra")
+        print("2. Papel")
+        print("3. Tijera")
+        print("4. Salir")
+       
+        eleccion_usuario = input("Ingresa el número de tu elección: ")
+
+        if eleccion_usuario == '4':
+            print("¡Gracias por jugar!")
+            break
+        
+        if eleccion_usuario not in ['1', '2', '3']:
+            print("Elección inválida. Inténtalo de nuevo.")
+            continue
+        eleccion_usuario = int(eleccion_usuario) - 1
+        eleccion_usuario_texto = opciones[eleccion_usuario]
+        eleccion_computadora = random.choice(opciones)
+
+        print(f"\nTú elegiste: {eleccion_usuario_texto}")
+        print(f"La computadora eligió: {eleccion_computadora}")
+
+        if eleccion_usuario_texto == eleccion_computadora:
+            print("---Es un empate!---")
+        elif (eleccion_usuario_texto == "piedra" and eleccion_computadora == "tijera") or \
+             (eleccion_usuario_texto == "papel" and eleccion_computadora == "piedra") or \
+             (eleccion_usuario_texto == "tijera" and eleccion_computadora == "papel"):
+            print("---¡Ganaste!---")
+        else:
+            print("---Perdiste---")
+
+
+
+piedra_papel_tijera()
